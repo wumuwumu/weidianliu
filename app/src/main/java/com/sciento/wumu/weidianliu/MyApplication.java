4b0a6de003bc2db644a190ca8c3efdbd6c1eb034
wumuwumu <2232728687@qq.com>
2017-10-29 20:15:11 +0800

    修复定时的问题，长按清零

---
+++ app/src/main/java/com/sciento/wumu/weidianliu/MyApplication.java
@@ -0,0 +1,22 @@
+package com.sciento.wumu.weidianliu;
+
+import android.app.Application;
+
+import com.squareup.leakcanary.LeakCanary;
+
+/**
+ * Created by wumu on 17-8-22.
+ */
+
+public class MyApplication extends Application {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        if (LeakCanary.isInAnalyzerProcess(this)) {
+            // This process is dedicated to LeakCanary for heap analysis.
+            // You should not init your app in this process.
+            return;
+        }
+        LeakCanary.install(this);
+    }
+}
